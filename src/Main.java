a110c349c7a81d7ee8a6daca55f192ea3d1307a5
SaaqibShaikh <113772083+SaaqibShaikh@users.noreply.github.com>
2023-02-10 21:52:02 +0530

    A basic code written in Java

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner pen=new Scanner(System.in);
+        float a= pen.nextFloat();
+        System.out.println(a);
+    }
+}
