cdb39a8fd669a8a4239824fa02bf21f9971ef11b
SaaqibShaikh <113772083+SaaqibShaikh@users.noreply.github.com>
2023-02-10 21:49:04 +0530

    A basic code written in Java

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,9 @@ public class Main {
         Scanner pen=new Scanner(System.in);
         float a= pen.nextFloat();
         System.out.println(a);
+        float b= pen.nextFloat();
+        System.out.println((b));
+        a = (a + b);
+        System.out.println(a);
     }
 }
